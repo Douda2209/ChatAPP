8ec5b9a86cdf413da95500f46632f83cb2b6990a
Dhia <dhia.moula@tuhh.de>
2025-09-13 15:31:14 +0200

    added the initial coomponents of the api call url and key

--- src/main/java/com/ai/gemini_chat/QnAService.java
+++ src/main/java/com/ai/gemini_chat/QnAService.java
@@ -1,12 +1,24 @@
 package com.ai.gemini_chat;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service //Spring Service: spring managed component
 public class QnAService {
-    public String getAnswer(String question) {
 
+    // Access to API KEY and URL (Gemini)
+    @Value("${gemini.api.url}")// get the value from the application propreties
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+    public String getAnswer(String question) {
+        // Construct the request payload
+
+        // Make API Call
+
+        // Return the response
         return "none initially";
     }
 }
